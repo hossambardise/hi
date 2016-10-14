3ea846b311c36d1bbd526a0d1519317de74294df
hossambardise <princegame73@gmail.com>
2016-10-15 01:44:06 +0200

    enemy move

---
+++ game1/Assets/Enemymove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemymove : MonoBehaviour {
+    Transform Player;
+    NavMeshAgent nav;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(Player.position);
+	
+	}
+}
